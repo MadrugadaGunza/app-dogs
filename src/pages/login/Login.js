0acf56ca032ecdf5e8545f283a9f2fb2a864ea08
Madrugada de Carvalho <madrugadaoca@gmail.com>
2024-10-20 05:05:58 +0100

    feat: add image background in login page

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,3 +1,6 @@
+// styles
+import styles from './Login.module.css';
+// dependencies
 import React from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import { UserContext } from '../../contexts/UserContext';
@@ -12,12 +15,16 @@ const Login = () => {
     if (login === true) return <Navigate to='/conta' />;
 
     return (
-        <Routes>
-            <Route path='/' element={<LoginForm />} />
-            <Route path='/criar' element={<LoginCreate />} />
-            <Route path='/perdeu' element={<LoginPasswordLost />} />
-            <Route path='/resetar' element={<LoginPasswordReset />} />
-        </Routes>
+        <section className={styles.login}>
+            <div className={styles.forms}>
+                <Routes>
+                    <Route path='/' element={<LoginForm />} />
+                    <Route path='/criar' element={<LoginCreate />} />
+                    <Route path='/perdeu' element={<LoginPasswordLost />} />
+                    <Route path='/resetar' element={<LoginPasswordReset />} />
+                </Routes>
+            </div>
+        </section>
     )
 }
 
