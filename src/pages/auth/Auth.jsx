8dc92c288845b4096419f18fd58e80f8aa386f35
Madrugada de Carvalho <madrugadaoca@gmail.com>
2025-07-19 12:31:14 +0100

    add pages of logic auth

--- src/pages/auth/Auth.jsx
+++ src/pages/auth/Auth.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Login from './login/Login'
+import Create from './create/Create'
+import Lost from './lost/Lost'
+import Reset from './reset/Reset'
 
 const Auth = () => {
     return (
         <Routes>
             <Route path='/' element={<Login />} />
+            <Route path='/criar' element={<Create />} />
+            <Route path='/perdeu' element={<Lost />} />
+            <Route path='/resetar' element={<Reset />} />
         </Routes>
     )
 }
---
+++ src/pages/auth/create/Create.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Create = () => {
+    return (
+        <div>
+            <h1>create county</h1>
+        </div>
+    )
+}
+
+export default Create
